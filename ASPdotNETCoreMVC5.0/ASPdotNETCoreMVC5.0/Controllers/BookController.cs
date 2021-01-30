226ba8dd7c6ca50316b07df976bd700d41a608de
BİROL AY <ayd.birol1@outlook.com>
2021-01-30 22:19:45 +0300

    Insert data in database using entity framework core

--- ASPdotNETCoreMVC5.0/ASPdotNETCoreMVC5.0/Controllers/BookController.cs
+++ ASPdotNETCoreMVC5.0/ASPdotNETCoreMVC5.0/Controllers/BookController.cs
@@ -13,9 +13,9 @@ namespace ASPdotNETCoreMVC5._0.Controllers
     {
         private readonly BookRepository _bookRepository = null;
 
-        public BookController()
+        public BookController(BookRepository bookRepository)
         {
-            _bookRepository = new BookRepository();
+            _bookRepository = bookRepository;
         }
 
         public ViewResult GetAllBooks()
@@ -36,14 +36,22 @@ namespace ASPdotNETCoreMVC5._0.Controllers
             return _bookRepository.SearchBook(bookName, authorName);
         }
 
-        public ViewResult AddNewBook()
+        public ViewResult AddNewBook(bool isSuccess = false ,int bookId = 0)
         {
+            ViewBag.IsSuccess = isSuccess;
+            ViewBag.BookId = bookId;
             return View();
         }
 
         [HttpPost]
-        public ViewResult AddNewBook(BookModel bookModel)
+        public IActionResult AddNewBook(BookModel bookModel)
         {
+            int id = _bookRepository.AddNewBook(bookModel);
+            
+            if(id > 0)
+            {
+                return RedirectToAction(nameof(AddNewBook), new { isSuccess = true , bookId = id });
+            }
             return View();
         }
     }
