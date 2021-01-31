b1a6a98a72b6a01d54de116aaf5bef5236ec50d8
BİROL AY <ayd.birol1@outlook.com>
2021-01-31 23:38:27 +0300

    Model validations

--- ASPdotNETCoreMVC5.0/ASPdotNETCoreMVC5.0/Data/Books.cs
+++ ASPdotNETCoreMVC5.0/ASPdotNETCoreMVC5.0/Data/Books.cs
@@ -9,18 +9,15 @@ namespace ASPdotNETCoreMVC5._0.Data
     public class Books
     {
         public int Id { get; set; }
-        [StringLength(100, MinimumLength = 5)]
-        [Required(ErrorMessage ="Please enter the title of the book")]
+        
         public string Title { get; set; }
-        [Required(ErrorMessage ="Please enter the author name")]
+        
         public string Author { get; set; }
-        [StringLength(500)]
-        [Required]
+        
         public string Description { get; set; }
         public string Category { get; set; }
         public string Language { get; set; }
-        [Required(ErrorMessage ="Please enter the total pages")]
-        [Display(Name ="Total pages of book")]
+        
         public int TotalPages { get; set; }
         public DateTime? CreatedOn { get; set; }
         public DateTime? UpdatedOn { get; set; }
