d894e182333445434c7cf25de28e0d67ecbcee0c
BİROL AY <ayd.birol1@outlook.com>
2021-02-02 23:42:05 +0300

    Get data from database and use in View Component

--- ASPdotNETCoreMVC5.0/ASPdotNETCoreMVC5.0/Components/TopBooksViewComponent.cs
+++ ASPdotNETCoreMVC5.0/ASPdotNETCoreMVC5.0/Components/TopBooksViewComponent.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPdotNETCoreMVC5._0.Repository;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,16 @@ namespace ASPdotNETCoreMVC5._0.Components
 {
     public class TopBooksViewComponent :ViewComponent
     {
+        private readonly BookRepository _bookRepository;
+
+        public TopBooksViewComponent(BookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
          public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var books = await _bookRepository.GetTopBooksAsync();
+            return View(books);
         }
     }
 }
